352ef77303a7df7fcd4e04a0eb97bf64166f15d5
jhk742 <jhk742@nyu.edu>
2024-02-20 15:21:39 +0900

    makes use of localStorage item, User, to load correct page depending on whether a user is logged-in or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { AuthContext } from './context/AuthContext'
 import Register from './pages/Register'
 import Home from './pages/Home'
 
+
 export default function App() {
+
+  const { user } = useContext(AuthContext)
+
   return (
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
       </Routes>
   )
 }
