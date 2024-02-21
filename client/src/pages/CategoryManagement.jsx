3178059a27970bcb41c63fcb3bd97c289b29f616
jhk742 <jhk742@nyu.edu>
2024-02-21 16:56:10 +0900

    clicking on a category renders the data from the specified row onto the input fields for deletion/update

--- client/src/pages/CategoryManagement.jsx
+++ client/src/pages/CategoryManagement.jsx
@@ -8,11 +8,14 @@ export default function CategoryManagement() {
     const [isAddingLoading, setIsAddingLoading] = useState(false)
     const [addingCategoryError, setAddingCategoryError] = useState(null)
     const [categoryData, setCategoryData] = useState({
+        _id: "",
         name: "",
         description: ""
     })
 
-    // console.log(categories)
+    const [selectedRow, setSelectedRow] = useState(null)
+
+    console.log(categoryData._id, categoryData.name, categoryData.description)
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -21,16 +24,6 @@ export default function CategoryManagement() {
         }
         fetchCategories()
     }, [])
-
-    const categoriesList = categories.map((category, index) => {
-        const { name, description } = category
-        return (
-            <tr key={index}>
-                <td>{name}</td>
-                <td>{description}</td>
-            </tr>
-        )
-    })
 
     const handleChange = (e) => {
         const { name, value } = e.target
@@ -47,10 +40,19 @@ export default function CategoryManagement() {
 
         try {
             const res = await postRequest(`${baseUrl}/categories/addItem`, JSON.stringify(categoryData))
+            
+            console.log(res)
+            
             if (res.error) {
                 return setAddingCategoryError(res)
             }
             setCategories(prev => [...prev, res])
+            //to clear the input fields
+            setCategoryData({
+                _id: "",
+                name: "",
+                description: ""
+            })
         } catch (error) {
             console.error("Error adding category: ", error)
             setAddingCategoryError(error.message)
@@ -58,6 +60,48 @@ export default function CategoryManagement() {
             setIsAddingLoading(false)
         }
     }
+
+    const handleUpdate = async (e) => {
+        e.preventDefault()
+    }
+
+    //this function changes state, causing a change in the rendering of the component
+    //thus, react will re-render the comp to reflect the updated state including the
+    //categoriesList jsx expression
+    const handleRowClick = (e, category) => {
+        const { _id, name, description } = category
+
+        //if the row is already selected, 
+        setSelectedRow(prev => prev === _id ? null : _id)
+
+        //setCategoryData (so as to bring in the information to the input fields)
+        setCategoryData({
+            _id,
+            name,
+            description
+        })
+    }
+
+    const categoriesList = categories.map((category, index) => {
+        const { _id, name, description } = category
+
+        //determine if the current row is clicked
+        const isClicked = selectedRow === _id
+
+        //define the class name based on the clicked state
+        const rowClassName = isClicked ? 'clicked-row' : ""
+
+        return (
+            <tr 
+                key={index} 
+                className={`category-table-row ${rowClassName}`}
+                onClick={(e) => handleRowClick(e, category)}
+            >
+                <td>{name}</td>
+                <td>{description}</td>
+            </tr>
+        )
+    })
 
     return (
         <div className="CategoryManagement">
@@ -78,7 +122,6 @@ export default function CategoryManagement() {
                         </table>
                     </div>
                     <div className="categories-functions">
-                        STUFF
                         <form 
                             className="categories-form"
                             onSubmit={handleOnSubmit}
@@ -105,8 +148,14 @@ export default function CategoryManagement() {
                             </div>
                             <button 
                                 type="submit"
+                                disabled={selectedRow}
                             >
                                 {isAddingLoading ? "Adding Category" : "Add Category"}
+                            </button>
+                            <button
+                                disabled={!selectedRow}
+                            >
+                                Update Category
                             </button>
                         </form>
                     </div>
